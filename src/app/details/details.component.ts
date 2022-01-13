b79fb9663375b3a06ba0953cd479081003d10532
Arnoldblair <arnoldblair.munene@student.moringaschool.com>
2022-01-13 12:49:56 +0300

    added component quote

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
-  selector: 'app-details',
-  templateUrl: './details.component.html',
-  styleUrls: ['./details.component.css']
+  selector: 'app-quote-details',
+  templateUrl: './quote-details.component.html',
+  styleUrls: ['./quote-details.component.css']
 })
-export class DetailsComponent implements OnInit {
+export class QuoteDetailsComponent implements OnInit {
+  @Input() quote!: Quote;
 
   constructor() { }
 
